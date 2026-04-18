22c3d10dd6e435d3277a4a440e16b5352f17ed57
Dmytro Riabov <fizzyelven@gmail.com>
2026-04-18 18:44:03 +0300

    fix filtration by category other

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,11 +1,11 @@
-import {type FilterConfig, FILTERS} from "../types/types.ts";
+import {type FilterConfig} from "../types/types.ts";
 
 export function filterArray<T extends {}>(array: T[], filters: FilterConfig<T>[]) {
     if (array.length > 0 && filters.length > 0) {
         return [...array].filter(item => {
             return filters.every((filter) => {
-                if (filter.field === FILTERS.OTHER) {
-                    return item.hasOwnProperty(filter.value)
+                if (filter.value in item) {
+                    return item[filter.value as keyof typeof item];
                 }
                 return item[filter.field as keyof T] === filter.value
             })
